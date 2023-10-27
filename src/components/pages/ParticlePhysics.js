f41591921d7fa9d2afc5ec9b80fc2524dc7bc053
antonov737 <antonov737x@gmail.com>
2023-10-27 19:32:16 +0100

    Added introduction to particle physics page

--- src/components/pages/ParticlePhysics.js
+++ src/components/pages/ParticlePhysics.js
@@ -1,17 +1,25 @@
 import React from "react";
 import Header from "../layout/Header";
 import "./ParticlePhysics.css";
-import Box from "../layout/Box.js"
+import Box from "../layout/Box";
 
 const ParticlePhysics = () => {
     return <div>
         <Header />
         <div className="layout">
-            <h1>The Wonders of Particle Physics!</h1>
-             <div className="items">
-                <Box>
-                    Here is some example text to test the functionality of this box.
-                </Box>
+             <div className="flexColumn">
+                <h1>The Importance Particle Physics in Computer Science!</h1>
+                <h2> Introduction </h2>
+                <p>
+                    Learning/simulating particle physics is essential to us humans' understanding
+                    of the universe around us. Some theorems about particle physics can be proven in real-world
+                    experiments, but a lot of them cannot be. This is where the importance of computer
+                    science comes into play in simulating the universe around us. By simulating complex
+                    models with the use of computational power, we are able to prove/conseptualise
+                    theorems and/or phenomenoms in computer science that just would simply not be
+                    possible without computation.
+
+                </p>
             </div> 
         </div>
     </div>
