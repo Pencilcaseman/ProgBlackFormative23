ef5aba7f369a5896c5cab5cecd09bbc91888d69c
Jelahu <x@gmail.com>
2023-10-28 19:55:03 +0100

    Updated 404 Page

--- src/components/pages/PageNotFound.js
+++ src/components/pages/PageNotFound.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-
+import error from "../layout//404.webp";
 import Button from "../layout/Button";
 import './PageNotFound.css';
 
@@ -14,6 +14,7 @@ const PageNotFound = () => {
 
     return <span className="error">
         <p>Error: 404!</p>
+        <img src={error} alt=""/>
         <p>Page Not Found</p>
         <Button color="white" name="Go Back Home" onClick={homePageHandler}/>
     </span>
