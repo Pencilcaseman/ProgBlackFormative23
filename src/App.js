ea1a47e483e42bcf208f02e15965aa7ece651aed
Jelahu <x@gmail.com>
2023-10-28 14:51:55 +0100

    Updated CSS Styling

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import {
 } from 'react-router-dom';
 import './App.css';
 
-import Home from './components/pages/Home';
 import ArtificialIntelligence from "./components/pages/ArtificialIntelligence";
+import Home from './components/pages/Home';
 import PageNotFound from './components/pages/PageNotFound';
 import ParticlePhysics from './components/pages/ParticlePhysics';
 
 function App() {
   return (
+    <div className='App'>
     <Router>
       <Routes>
         <Route path='/home' element={<Home />} />
@@ -19,6 +20,7 @@ function App() {
         <Route path='/particlephysics' element={<ParticlePhysics />} />
       </Routes>
     </Router>
+    </div>
   );
 }
 
