2bdf7009374396de9e7ebace961ede8ca43dd254
antonov737 <antonov737x@gmail.com>
2023-10-31 11:30:18 +0000

    added path for new pp citation page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ArtificialIntelligence from "./components/pages/ArtificialIntelligence";
 import Home from './components/pages/Home';
 import PageNotFound from './components/pages/PageNotFound';
 import ParticlePhysics from './components/pages/ParticlePhysics';
+import ParticlePhysicsCitations from './components/pages/ParticlePhysicsCitations';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path='/artificial-intelligence' element={<ArtificialIntelligence />} />
         <Route path='*' element={<PageNotFound />} />
         <Route path='/particlephysics' element={<ParticlePhysics />} />
+        <Route path='/particlephysics_citations' element={<ParticlePhysicsCitations />} />
       </Routes>
     </Router>
     </div>
