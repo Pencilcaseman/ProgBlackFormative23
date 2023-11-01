eded9f3ead9ea4e4d5a49f38cf8f8d7c313d081e
Toby Davis <pencilcaseman@gmail.com>
2023-11-01 23:56:03 +0000

    Add more contact info

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,6 +3,7 @@ import Box from "../layout/Box";
 import Button from "../layout/Button";
 import Header from "../layout/Header";
 import "./PageStyling.css";
+import "./Home.css"
 
 const Home = () => {
     return <div>
@@ -13,23 +14,43 @@ const Home = () => {
             <div className="home-items">
                 <Box>
                     <h2>Toby Davis</h2>
-                    {/* https://bobbyhadz.com/blog/react-button-link */}
-                    {/* For Help on getting the button to navigate to an external page */}
-                    <a href="https://github.com/Pencilcaseman" target="_blank" rel="noopener noreferrer">
-                        <Button color="blue" name="Github"/>
-                    </a>
+                    <div class="flexRow">
+                        <a href="https://github.com/Pencilcaseman" target="_blank" rel="noopener noreferrer">
+                            <Button color="blue" name="Github"/>
+                        </a>
+
+                        <a href="https://tobydavis.dev" target="_blank" rel="noopener noreferrer">
+                            <Button color="blue" name="Personal Website"/>
+                        </a>
+
+                        <a href="https://www.linkedin.com/in/toby-davis-codes/" target="_blank" rel="noopener noreferrer">
+                            <Button color="blue" name="LinkedIn"/>
+                        </a>
+                    </div>
                 </Box>
                 <Box>
                     <h2>Anton Egorov</h2>
-                    <a href="https://github.com/antonov737" target="_blank" rel="noopener noreferrer">
-                        <Button color="blue" name="Github"/>
-                    </a>
+                    <div class="flexRow">
+                        <a href="https://github.com/antonov737" target="_blank" rel="noopener noreferrer">
+                            <Button color="blue" name="Github"/>
+                        </a>
+
+                        <a href="https://www.linkedin.com/in/anton-egorov-310a6a22a" target="_blank" rel="noopener noreferrer">
+                            <Button color="blue" name="LinkedIn"/>
+                        </a>
+                    </div>
                 </Box>
                 <Box>
                     <h2>Lewis Hurd</h2>
-                    <a href="https://github.com/jelahu" target="_blank" rel="noopener noreferrer">
-                        <Button color="blue" name="Github"/>
-                    </a>
+                    <div class="flexRow">
+                        <a href="https://github.com/jelahu" target="_blank" rel="noopener noreferrer">
+                            <Button color="blue" name="Github"/>
+                        </a>
+
+                        <a href="https://www.linkedin.com/in/lewis-hurd-34829b257/" target="_blank" rel="noopener noreferrer">
+                            <Button color="blue" name="LinkedIn"/>
+                        </a>
+                    </div>
                 </Box>
                 <Box>
                     <h2>
